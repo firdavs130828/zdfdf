c6b81025830d09f6a87bb87ec01fef9dde8a0acb
firdavs130828 <arabovfirdavs7@gmail.com>
2025-08-01 11:59:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+prompt("ismingizni  kiriting")
+prompt("familyangizni kiriting")
+prompt("yoshingiz")
+prompt("turar joyingiz")
+prompt("telefon raqamingiz")
+prompt("sizning google akauntingiz")
+prompt("ota-onangizni telefon raqami")
+
+
+
+
+
+
